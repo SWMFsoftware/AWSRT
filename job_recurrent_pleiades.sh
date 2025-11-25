0bd7efe37a306109a479e0e51622323c8fb34587
Igor V. Sokolov <igorsok@umich.edu>
2025-11-24 21:38:43 -0500

    New job for recurrent work on Pleiades

--- job_recurrent_pleiades.sh
+++ job_recurrent_pleiades.sh
@@ -1,146 +1,95 @@
-#!/bin/tcsh
-
-### SWMF solar-helio realtime simulation
-### Gergely Koban for Pleiades
-### Based on work from Igor Sokolov
-###########################################
-
-#PBS -S /bin/tcsh
-#PBS -N AWSRT
-#PBS -l select=8:ncpus=40:model=sky_ele
-### PBS -q long
-### PBS -q devel
-#PBS -l walltime=6:30:00
-#PBS -j oe
-#PBS -m e
-#PBS -W group_list=s2994
-# cd $PBS_O_WORKDIR
-# For PGI compilers uncomment the 3 lines below
-#module purge; module load comp-pgi mpi-hpe idl python
-#setenv MPICC_CC pgcc
-#setenv MPICXX_CXX pgc++>
-setenv MPI_TYPE_DEPTH 20
-
-
+#!/bin/bash
+#PBS -S /bin/bash
+#PBS -N SWMF_MFLAMPA_RT
+#PBS -W group_list=s2157
+#PBS -o /dev/null
+#PBS -e /dev/null
+# To run on the 28-core Electra Broadwell nodes (128GB/node or 4.5GB/core)
+#PBS -l select=16:ncpus=28:model=bro_ele
+#PBS -q long
+#PBS -l walltime=60:00:00 
 ####################################################
 # Loading the modules
-source /usr/share/Modules/init/tcsh
-###module purge
-
-setenv MODULES_VERBOSITY concise
-module load comp-intel gcc/9.3 mpi-hpe/mpt idl
-
-setenv MPICC icc
-setenv MPICXX_CXX icpc
-
-alias rm /bin/rm -i                     # prompting remove
-alias mv /bin/mv -i                     # prompting move
-alias cp /bin/cp -i                     # prompting copy
-
+# Loading the modules
+source /usr/share/Modules/init/bash
+module purge
+module load comp-intel/2020.4.304
+module load mpi-hpe/mpt.2.30
+module load gcc/9.3
 module use -a /swbuild/analytix/tools/modulefiles
 module load miniconda3/v4
-
+module load cuda/11.0 
+module use -a /nasa/modulefiles/testing
+module load python3/3.8.8
 #Fix stack size issue
-#ulimit -s unlimited
-
-setenv SWMF_dir /nobackupp28/gkoban/SWMF_AWSRT/SWMF
-setenv RUNDIR ${SWMF_dir}/run_realtime
-### echo "SWMF dir: $SWMF_dir" >> $RUNDIR/debug.log
-### echo "Run dir: $RUNDIR" >> $RUNDIR/debug.log
-
+ulimit -s unlimited
+SWMF_dir=/home4/mpetrenk/MODELS/SH/SWMF_solar/SWMF_MFLAMPA_DEV
+RUNDIR=/nobackupp28/isokolov/run_realtime
 #
 ##### Remove stop files
 rm -f $RUNDIR/AWSOMRT.STOP $RUNDIR/SC/AWSOMRT.STOP 
-
-foreach iDay ( 1 2 3 4 5 6 7 )
-    foreach M ( AM PM )
-        foreach iHour ( 1 2 3 4 5 6 7 8 9 10 11 12 )
-            cd $RUNDIR
-
-            # Check for STOP file
-            if ( -f AWSOMRT.STOP ) then
-                ./PostProc.pl -M -cat RESULTS_`date +%y%m%d_%H%M`
-		### echo "Find AWSOMRT.STOP in $RUNDIR"  >> $RUNDIR/debug.log
-                exit 0
-            endif
-
-            mv PARAM.in PARAM.in_`date +%y%m%d_%H%M`
-            rm -f PARAM.in_orig_
-            rm -f harmonics_bxyz.out
-            mv harmonics_new_bxyz.out harmonics_bxyz.out
-
-            cd $RUNDIR/SC
-            rm -f STARTMAGNETOGRAMTIME.in PARAM.tmp *.fits.gz *.fits
-            rm -f harmonics.log* fitsfile_01.out endmagnetogram*
-            mv -f ENDMAGNETOGRAMTIME.in STARTMAGNETOGRAMTIME.in
-
-	    cp $SWMF_dir/SUBMISSION_DATA/fitsfile.fits $RUNDIR/SC/fitsfile.fits
-            cd $RUNDIR/SC
-            mv *.fits endmagnetogram
-
-            python3 remap_magnetogram.py endmagnetogram fitsfile
-
-            ./HARMONICS.exe > harmonics.log_`date +%y%m%d_%H%M`
-            mv MAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in
-
-	   	
-            # While the diff is empty
-            while ( "`diff STARTMAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in`" == "" )
-                # STOP file check
-                if ( -f AWSOMRT.STOP ) then
-                    rm -f ENDMAGNETOGRAMTIME.in
-                    mv STARTMAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in
-		    ### echo "Find AWSOMRT.STOP in $RUNDIR/SC"  >> $RUNDIR/debug.log
-                    exit 0
-                endif
-
+for iDay in 1 2
+do
+    for M in AM PM
+    do
+	for iHour in 1 2 3 4 5 6 7 8 9 10 11 12
+	do
+	    cd $RUNDIR
+	    if [ -f "AWSOMRT.STOP" ]; then
+		./PostProc.pl -M -cat RESULTS_`date +%y%m%d_%H%M`
+		rm -f AWSOMRT.STOP
+		exit 0
+	    fi
+	    mv PARAM.in PARAM.in_`date +%y%m%d_%H%M`
+	    rm -f harmonics_bxyz.out
+	    mv  harmonics_new_bxyz.out harmonics_bxyz.out
+	    cd $RUNDIR/SC
+	    rm -f STARTMAGNETOGRAMTIME.in PARAM.tmp *.fits.gz *.fits
+	    rm -f harmonics.log* fitsfile_01.out endmagnetogram*
+	    mv ENDMAGNETOGRAMTIME.in STARTMAGNETOGRAMTIME.in
+	    python3  $SWMF_dir/AWSRT/get_magnetogram_pleiades.py
+	    cd $RUNDIR/SC
+	    tar -xzvf submission.tgz
+	    mv *.fits endmagnetogram
+	    python3 remap_magnetogram.py endmagnetogram fitsfile
+	    ./HARMONICS.exe >harmonics.log_`date +%y%m%d_%H%M`
+	    mv MAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in
+	    while [ "$( diff STARTMAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in )" == "" ]
+	    do
+		rm -f ENDMAGNETOGRAMTIME.in
+		if [ -f "AWSOMRT.STOP" ]; then
+		    mv STARTMAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in
+		    echo "Find AWSOMRT.STOP in $RUNDIR/SC"
+		    exit 0
+		fi
 		sleep 300
-		
-		cp $SWMF_dir/SUBMISSION_DATA/fitsfile.fits $RUNDIR/SC/fitsfile.fits
-
-                cd $RUNDIR/SC
-                mv *.fits endmagnetogram
-
-                python3 remap_magnetogram.py endmagnetogram fitsfile
-
-                ./HARMONICS.exe > harmonics.log_`date +%y%m%d_%H%M`
-                mv MAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in
-            end
-
-            cp -f $SWMF_dir/AWSRT/PARAM.in.realtime.restart PARAM.tmp
-
-            # Convert PARAM.tmp to PARAM.in
-            $SWMF_dir/share/Scripts/ParamConvert.pl PARAM.tmp ../PARAM.in
-
-            # Test PARAM.in
-            cd $SWMF_dir
-            Scripts/TestParam.pl -F $RUNDIR/PARAM.in
-
-            cd $RUNDIR
-	    ### echo "This is when the simulation starts"  >> $RUNDIR/debug.log
-	    mpiexec -n 128 ./SWMF.exe > runlog_`date +%y%m%d_%H%M`
-
-            if ( ! -f SWMF.SUCCESS ) then
-                rm -f harmonics_new_bxyz.out
-                mv harmonics_bxyz.out harmonics_new_bxyz.out
-                rm -f SC/ENDMAGNETOGRAMTIME.in
-                mv SC/STARTMAGNETOGRAMTIME.in SC/ENDMAGNETOGRAMTIME.in
-                exit 0
-            endif
-
-            ./PostProc.pl -n=16 > PostProc.log_`date +%y%m%d_%H%M`
-
-            cat IH/IO2/sat_earth_*.sat > sat_earth.sat
-            cat IH/IO2/sat_sta_*.sat > sat_sta.sat
-
-            rm -rf RESTART_n000000
-
-            ./Restart.pl -v
-
-        end
-    end
-end
-
+		python3  $SWMF_dir/AWSRT/get_magnetogram_pleiades.py
+		cd $RUNDIR/SC
+		tar -xzvf submission.tgz
+		mv *.fits endmagnetogram
+		python3 remap_magnetogram.py endmagnetogram fitsfile
+		./HARMONICS.exe >harmonics.log_`date +%y%m%d_%H%M`
+		mv MAGNETOGRAMTIME.in ENDMAGNETOGRAMTIME.in
+	    done
+	    cp $SWMF_dir/AWSRT/PARAM.in.restart.pleiades PARAM.tmp
+	    #Convert it as PARAM.in
+	    $SWMF_dir/share/Scripts/ParamConvert.pl PARAM.tmp ../PARAM.in
+	    cd $RUNDIR
+	    mpiexec -n 448 ./SWMF_solar.exe > runlog_`date +%y%m%d_%H%M`
+	    if [ ! -f SWMF.SUCCESS ]; then
+		rm -f harmonics_new_bxyz.out
+		mv harmonics_bxyz.out harmonics_new_bxyz.out
+		rm -f SC/ENDMAGNETOGRAMTIME.in
+		mv SC/STARTMAGNETOGRAMTIME.in SC/ENDMAGNETOGRAMTIME.in
+		exit 0
+	    fi
+	    ./PostProc.pl -n=16 >PostProc.log_`date +%y%m%d_%H%M`
+	    cat IH/IO2/sat_earth_*.sat>sat_earth.sat
+	    cat IH/IO2/sat_sta_*.sat>sat_sta.sat
+	    rm -rf RESTART_n000000
+	    ./Restart.pl -v
+	done
+    done
+done
 ./PostProc.pl -M -cat RESULTS_`date +%y%m%d_%H%M`
-
 exit 0
