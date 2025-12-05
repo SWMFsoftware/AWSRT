cbd88e0273e566699a6671e28ed778a2f70eeac1
Igor V. Sokolov <igorsok@umich.edu>
2025-12-05 04:05:55 -0500

    Development

--- job_recurrent_cron.sh
+++ job_recurrent_cron.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Test version for recurrent cron job script
+SWMF_DIR=/home4/mpetrenk/MODELS/SH/SWMF_solar/SWMF_MFLAMPA_DEV
+AWSRT=$SWMF_DIR/AWSRT
+module purge
+module load idl
+IDL_DIR=/nasa/idl/toss4/8.9/idl89
+echo "PATH: $PATH"
+export PATH="$PATH:$IDL_DIR/bin:$IDL_DIR/lib:$IDL_DIR/lib/utilities"
+echo "New PATH: $PATH"
+exit 0
 # Under developmen, more work is needed
 STOPTIME=$(date -d "23 hours 20 minutes" +%s)
 CURRENT_TIME=$(date +%s)
