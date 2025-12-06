c456f1d7352ca1d06a6d004ce5c7dbb5fc5ec271
Igor Sokolov <isokolov@pfe24.nas.nasa.gov>
2025-12-05 20:46:29 -0800

    Development of the visualization tool for RT

--- job_recurrent_cron.sh
+++ job_recurrent_cron.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 # Test version for recurrent cron job script
+####
+## SWMF directory
+####
 SWMF_DIR=/home4/mpetrenk/MODELS/SH/SWMF_solar/SWMF_MFLAMPA_DEV
+####
+## Collection of real-time infrastructure scripts
+####
 AWSRT=$SWMF_DIR/AWSRT
+####
+## Load IDL and init it for BASH
+####
+source /usr/share/Modules/init/bash
+####
+## By default, the IDL is not loaded 
 module purge
 module load idl
+####
+## Now, the IDL is present in the derictory below
+####
 IDL_DIR=/nasa/idl/toss4/8.9/idl89
-echo "PATH: $PATH"
+####
+## Add IDL to PATH
+####
 export PATH="$PATH:$IDL_DIR/bin:$IDL_DIR/lib:$IDL_DIR/lib/utilities"
-echo "New PATH: $PATH"
+####
+## Set IDL_PATH
+export IDL_PATH="$SWMF_DIR/share/IDL/General:<IDL_DEFAULT>"
+export IDL_STARTUP="$SWMF_DIR/share/IDL/General/idlrc"
+source ${IDL_DIR}/bin/idl_setup.bash
+####
+## Declare and cleanup real-time run directory
+####
+RUN_DIR=/nobackupp28/isokolov/run_realtime
+rm -f $RUN_DIR/PostProc.log_* $RUN_DIR/PARAM.in_* $RUN_DIR/RESULTS/runlog_*
+cd $RUN_DIR
+idl $AWSRT/plot_2d_slice_movie.pro > log_2d_slice_movie
 exit 0
 # Under developmen, more work is needed
 STOPTIME=$(date -d "23 hours 20 minutes" +%s)
--- plot_2d_slice_movie.pro
+++ plot_2d_slice_movie.pro
@@ -1,40 +1,22 @@
-filename='./RESULTS/SC/x=0*n0*000*.outs'
-npict=1
-firstpict=61
-read_data
-func='{br}*r^2<0.2>(-0.2) by;bz ur uy;uz'
-savemovie='mp4'
-videorate=3
-videofile='x=0_range'
-!x.range=[-5,5]
-!y.range=[-5,5]
-animate_data
-animate_data
 
-
-filename='./RESULTS/SC/y=0*n0*000*.outs'
+filename='./RESULTS/IH/z=0*.outs'
 npict=1
-firstpict=61
-read_data
-func='{br}*r^2<0.2>(-0.2) bx;bz ur ux;uz'
-savemovie='mp4'
-videorate=3
-videofile='y=0_range'
-!x.range=[-5,5]
-!y.range=[-5,5]
-animate_data
-
-filename='./RESULTS/SC/z=0*n0*000*.outs'
-npict=1
-firstpict=61
 read_data
 func='{br}*r^2<0.2>(-0.2) bx;by ur ux;uy'
 savemovie='mp4'
-videorate=3
-videofile='z=0_range'
+videofile='z=0_IH'
+!x.range=[-220,220]
+!y.range=[-220,220]
+animate_data
+
+filename='./RESULTS/SC/z=0*.outs'
+npict=1
+read_data
+func='{br}*r^2<0.2>(-0.2) bx;by ur ux;uy'
+savemovie='mp4'
+videofile='z=0_SC'
 !x.range=[-5,5]
 !y.range=[-5,5]
 animate_data
-
 
 exit
