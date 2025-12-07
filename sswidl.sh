b2a07b3abebde898376f67deedb7e42ce7b94ba8
Igor V. Sokolov <igorsok@umich.edu>
2025-12-06 21:00:57 -0500

    Adapt sswidl; SSW environmental variable needs to be fixed

--- sswidl.sh
+++ sswidl.sh
@@ -1,12 +1,14 @@
 #!/bin/tcsh
 ################# inititalization of the ssw-idl
 ################# ## 
-################# tcsh
+################# tcsh (if in bash)
 ################# source AWSRT/sswidl.sh
 ################# ##
 ############ Machine-dependedent commands ###########
 ############ Path to the IDL directory
-setenv IDL_DIR /Applications/NV5/idl91/
+setenv IDL_DIR /nasa/idl/toss4/8.9/idl89
+############ Path to SWMF/IDL routines
+setenv IDL_PATH "/home4/mpetrenk/MODELS/SH/SWMF_solar/SWMF_MFLAMPA_DEV/share/IDL/General:<IDL_DEFAULT>"
 ############ Path to the installed Solar SoftWare (SSW)
 setenv SSW $HOME/ssw
 setenv SSW_INSTR "gen soho aia hmi xrt eit lasco secchi nrl festival sunspice"
