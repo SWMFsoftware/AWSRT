376465611a01c5fe89e8cece9e3cc2d14a2805a1
Igor V. Sokolov <igorsok@umich.edu>
2026-07-23 22:39:47 -0400

    run_SWMFAWSOM_CME_init.sh

---
+++ run_SWMFAWSOM_CME_init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+####################################################
+SWMF_dir=`pwd`
+echo "$SWMF_dir"
+module purge
+module load comp-intel
+module load mpi-hpe/mpt
+module load gcc/9.3
+cd CLEAR; make install; cd ../
+./Config.pl -uninstall
+./Config.pl -install #-compiler=ifortmpif90,iccmpicxx
+cd CLEAR; make install; cd ../
+./Config.pl -v=Empty,IH/BATSRUS,SC/BATSRUS,SP/MFLAMPA
+./Config.pl -o=SC:u=AwsomR,e=AwsomSA,ng=2,g=8,8,4
+./Config.pl -o=IH:u=AwsomR,e=AwsomSA,ng=2,g=4,4,4
+./Config.pl -o=SP:g=20000
+./Config.pl -s
+make GITINFO=NO -j
+make PIDL
+make td_compile
+rm -rf run
+make rundir
+exit 0
