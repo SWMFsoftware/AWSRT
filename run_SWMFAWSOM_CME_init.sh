735b36f3730089abc4ce02848988ebc779fd4f3f
Igor Sokolov <isokolov@athfe01.head.ath.nas.nasa.gov>
2026-07-28 12:11:09 -0700

    Version for sofieops

--- run_SWMFAWSOM_CME_init.sh
+++ run_SWMFAWSOM_CME_init.sh
@@ -8,7 +8,9 @@ module load mpi-hpe/mpt
 module load gcc/9.3
 cd CLEAR; make install; cd ../
 ./Config.pl -uninstall
-./Config.pl -install #-compiler=ifortmpif90,iccmpicxx
+HOME=/home6/sofieops
+export HOME
+./Config.pl -install -compiler=ifortmpif90,iccmpicxx
 cd CLEAR; make install; cd ../
 ./Config.pl -v=Empty,IH/BATSRUS,SC/BATSRUS,SP/MFLAMPA
 ./Config.pl -o=SC:u=AwsomR,e=AwsomSA,ng=2,g=8,8,4
@@ -16,6 +18,7 @@ cd CLEAR; make install; cd ../
 ./Config.pl -o=SP:g=20000
 ./Config.pl -s
 make GITINFO=NO -j
+cd $SWMF_dir/PT/MITTENS; make -j; cd $SWMF_dir
 make PIDL
 make td_compile
 rm -rf run
